33966f5af7271acbe3c32fc9df33da2a43d3f291
Davit Papoyan <109224121+Papoyan077@users.noreply.github.com>
2023-03-16 15:08:26 +0400

    asdsad

--- src/components/main/employees/Employees.jsx
+++ src/components/main/employees/Employees.jsx
@@ -1,9 +1,10 @@
 import React, {useMemo} from 'react';
-import { useNavigate } from "react-router-dom";
-import {Box, Button, IconButton, Tooltip} from "@mui/material";
+import {useNavigate} from "react-router-dom";
 import axios from "axios";
 import {useEffect, useState} from "react";
 import MaterialReactTable from 'material-react-table';
+import {ListItemIcon, MenuItem} from "@mui/material";
+import {AccountCircle} from "@mui/icons-material";
 let JWTTOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibG9naW4iOiJhZG1pbiIsImlhdCI6MTY3ODQ0NjU1Mn0.vsg37gZ-pPRq4qDKrTg9mswSuZ3Ij1RjRBiJ9mafig4';
 
 const Employees = () => {
@@ -54,8 +55,10 @@ const Employees = () => {
     if(empdata===null){
         return null
     }
-    return <MaterialReactTable columns={columns} data={empdata.data}
-    />;
+    return <MaterialReactTable
+        data={empdata.data}
+        columns={columns}
+    />
 
 };
 
